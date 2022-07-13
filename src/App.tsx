b0ce0be3efe6f19bbdfcb57ca8f4b5efb3f86c4e
Alexanda <procoderfransi@gmail.com>
2022-07-13 06:42:50 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
-import {Button} from "@mantine/core";
+import {Button, Paper, Text, Loader, MantineProvider, ColorSchemeProvider, ColorScheme} from "@mantine/core";
+import {Cards} from "./Components/Cards";
+import {LightDarkButton} from "./Components/LightDarkButton";
+import {useHotkeys, useLocalStorage} from "@mantine/hooks";
+import {NavBar} from "./Components/NavBar";
 
 function App() {
+    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+        key: 'mantine-color-scheme',
+        defaultValue: 'light',
+        getInitialValueInEffect: true,
+    });
+
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+    useHotkeys([['mod+J', () => toggleColorScheme()]]);
   return (
-    <div className="App">
-      <Button>Hellow world!</Button>
+    <div >
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            <MantineProvider theme={{colorScheme}}>
+                <Paper  radius={0} style={{minHeight:"100vh",padding:2}}>
+                    <NavBar />
+                    <Cards/>
+                    <LightDarkButton />
+                </Paper>
+            </MantineProvider>
+
+        </ColorSchemeProvider>
+
     </div>
   );
 }
